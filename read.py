5476c9c89b2c49892a830fbf63bf197cdc0464f5
Zebin Zhou <zb.zhou0922@gmail.com>
2022-01-19 21:47:29 -0800

    calc avg length of reviews

--- read.py
+++ read.py
@@ -4,9 +4,12 @@ with open('reviews.txt', 'r') as f:
 	for line in f:	
 		data.append(line)
 		count += 1
-		if count % 1000 == 0:
+		if count % 100000 == 0:
 			print(len(data))
-print(len(data))
-print(data[0])
-print('--------------------------')
-print(data[1])
+print('The file is read. There are', len(data), 'reviews.')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+average = sum_len / len(data)
+print('The average length of all reviews are', average)
